881b2715bc64b9ba55452c955452d698b5075481
Chad Ostrowski <hi@chadoh.com>
2015-03-03 18:32:18 -0500

    Step 4: index action for restful route
    Lists all quotes from the db.
    
    http://phoenix.thefirehoseproject.com/4.html

--- web/controllers/quote_controller.ex
+++ web/controllers/quote_controller.ex
@@ -6,4 +6,10 @@ defmodule Splurty.QuoteController do
   def homepage(conn, _params) do
     render conn, "homepage.html"
   end
+
+  def index(conn, _params) do
+    conn
+    |> assign(:quotes, Repo.all(Splurty.Quote))
+    |> render("index.html")
+  end
 end
